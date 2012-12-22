f3f9ae11e8857d95fb8eb6b0251076c190410510
Mondkalb1@gmail.com <Mondkalb1@gmail.com@25e9bc7e-5778-1817-ca90-03ff761191b1>
2012-12-22 21:16:01 +0000

    [MBG] Basic AT Camping folder structure setup

---
+++ trunk/at_camping/modules.hpp
@@ -0,0 +1,12 @@
+class at_camping_init: Logic
+{
+	displayName		= "AT Camping: Init";
+	icon			= IMAGEPATH\icon\icon_camping_module_ca.paa;
+	picture			= IMAGEPATH\icon\icon_camping_module_ca.paa;
+	vehicleClass	= "Modules";
+
+	class Eventhandlers
+	{
+		// init = "call compile preprocessFile '\at\at_camping\data\script\at_camping_init.sqf'";
+	};
+};
